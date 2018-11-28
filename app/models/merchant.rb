f62bb3f92f68ff6a52ae648e3c998f4c43aafac6
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-11-27 21:22:51 -0700

    wrote model validations and nonfunctional most_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,17 @@
 class Merchant < ApplicationRecord
+  validates_presence_of :name
+
   has_many :invoices
   has_many :items
+
+  def self.most_revenue(x)
+    select('distinct merchants.*, sum(invoice_items.quantity * invoice_items.price) as revenue')
+      .joins(:items)
+      .joins('join invoice_items on items.id=invoice_items.item_id')
+      .joins('join invoices on invoices.id=invoice_items.invoice_id')
+      .group('merchants.id, invoice_items.id')
+      .order('revenue DESC')
+      .limit(x)
+
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+  end
+
   describe 'Relationships' do
     it { should have_many(:items) }
     it { should have_many(:invoices) }
   end
 
-  describe 'Instance Methods' do
+  describe 'Class Methods' do
 
     before(:each) do
-      
+      m_1, m_2, m_3 = create_list(:merchant, 3)
+      inv = create(:invoice, status: "shipped")
+      i_1 = create(:item, merchant: m_1)
+      ii_1 = create(:invoice_item, unit_price: 200, quantity: 3, item: i_1, invoice: inv)
+      i_2 = create(:item, merchant: m_1)
+      ii_2 = create(:invoice_item, unit_price: 300, quantity: 4, item: i_2, invoice: inv)
+      i_3 = create(:item, merchant: m_2)
+      ii_3 = create(:invoice_item, unit_price: 100, quantity: 4, item: i_3, invoice: inv)
+      i_4 = create(:item, merchant: m_3)
+      ii_4 = create(:invoice_item, unit_price: 100, quantity: 6, item: i_4, invoice: inv)
+    end
+
+    it 'returns top x merchants ranked by total revenue' do
+
+      top_merchants = Merchant.most_revenue(2)
+
+      expect(top_merchants.count).to eq(2)
+      expect(top_merchants[0].id).to eq(i_1.id)
+      expect(top_merchants[1].id).to eq(i_3.id)
     end
   end
 end
