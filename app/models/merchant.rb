7403dccd3f72935e4ecc106406c2078be2ffbb80
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-11-28 21:13:26 -0700

    most_revenue request with controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,13 +4,13 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
 
-  def self.most_revenue(x)
+  def self.most_revenue(quantity)
     select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
       .joins(:items)
       .joins('join invoice_items on items.id=invoice_items.item_id')
       .joins('join invoices on invoices.id=invoice_items.invoice_id')
       .group('merchants.id')
       .order('revenue DESC')
-      .limit(x)
+      .limit(quantity)
   end
 end
