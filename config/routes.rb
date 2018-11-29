c2508d88cfd50739842f0472da5f5263d321f64d
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-11-29 08:37:49 -0700

    random endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
-        get '/most_revenue', to: 'most_revenue#index'
         get '/random', to: 'random#show'
+        get '/most_revenue', to: 'most_revenue#index'
       end
       namespace :customers do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
       end
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
